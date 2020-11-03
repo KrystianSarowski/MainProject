365aa383f8d33bb905e8fd349d1d24a543a15938
KrystianSarowski <C00225629@itcarlow.ie>
2020-11-03 12:42:37 +0000

    Base Grid created, base level manager created, test draw function added to level manager to display the grid

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public MapGrid m_mapGrid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(m_mapGrid != null)
+        {
+            m_mapGrid.CreateMap();
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (m_mapGrid != null)
+        {
+            for (int x = 0; x < m_mapGrid.m_width; x++)
+            {
+                for (int y = 0; y < m_mapGrid.m_height; y++)
+                {
+                    Gizmos.color = (m_mapGrid.GetTile(x, y) == 1) ? Color.black : Color.white;
+                    Vector3 pos = new Vector3(-m_mapGrid.m_width / 2 + x + 0.5f, 0, -m_mapGrid.m_height / 2 + y + 0.5f);
+                    Gizmos.DrawCube(pos, Vector3.one);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MapGrid.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGrid : MonoBehaviour
+{
+    public int m_height;
+    public int m_width;
+
+    int[,] m_map;
+
+    public void CreateMap()
+    {
+        m_map = new int[m_width, m_height];
+
+        if (m_map.GetLength(0) == 0 || m_map.GetLength(1) == 1)
+        {
+            Debug.Log("Warning One of the dimensions has a size of 0");
+        }
+        else
+        {
+            for (int x = 0; x < m_width; x++)
+            {
+                for (int y = 0; y < m_height; y++)
+                {
+                    m_map[x, y] = 1;
+                }
+            }
+        }
+    }
+
+    public int GetTile(int t_xIndex, int t_yIndex)
+    {
+        if(m_map != null)
+        {
+            if (t_xIndex >= 0 || t_xIndex < m_width || t_yIndex >= 0 || t_yIndex < m_height)
+            {
+                return m_map[t_xIndex, t_yIndex];
+            }
+        }
+        return -1;
+    }
+
+    public void SetTile(int t_xIndex, int t_yIndex, int t_newTileValue)
+    {
+        m_map[t_xIndex, t_yIndex] = t_newTileValue;
+    }
+}
